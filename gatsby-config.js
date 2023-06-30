2375d6abaccdfb38396a8e2d17f8c50e7b5a1684
Roxanne Liu <roxanneliu819@gmail.com>
2023-06-30 23:35:14 +1000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `LaunchHub`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
