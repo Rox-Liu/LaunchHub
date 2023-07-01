5912d6725843e567797a43579728b4385326dadb
Dominic <dominicwong09@gmail.com>
2023-07-01 17:33:20 +1200

    More footer content

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -27,6 +27,12 @@ const Footer = () => {
                     <a href="#">LinkedIn</a>
                 </div>
             </div>
+
+            <div>
+                <p>
+                    We pay our respects to the traditional custodians of the lands on which we live and work, and to the traditional custodians of the lands and waters which we may visit upon in our work. We acknowledge their elders past and present. Indigenous sovereignty has never been ceded. It always was, and always will be Aboriginal land.
+                </p>
+            </div>
         </footer>
     )
 
