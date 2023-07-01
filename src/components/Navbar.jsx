3adf4aa289e11c1e398ca7d80ab0c4f3820c8187
Roxanne Liu <roxanneliu819@gmail.com>
2023-07-01 15:55:53 +1000

    navbar bug fixed, v1.2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,12 @@ const Navbar = () => {
     return (
         <nav>
             <div className="logo"></div>
-            <label htmlFor="checkbox-menu" className="label-menu"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg></label>
+            <label htmlFor="checkbox-menu" className="label-menu">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+                    <path d="M0 0h24v24H0V0z" fill="none"/>
+                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+                </svg>
+            </label>
             <input type="checkbox" id="checkbox-menu"></input>
             <div className="menu-list">
                 <div>
@@ -18,7 +23,13 @@ const Navbar = () => {
                     <Link to="/about">About Us</Link>
                 </div>
                 <div>
-                    <AnchorLink to="/#projects">Projects</AnchorLink>
+                    <AnchorLink to="/#services">Services</AnchorLink>
+                </div>
+                <div>
+                    <AnchorLink to="/#works">Works</AnchorLink>
+                </div>
+                <div>
+                    <AnchorLink to="/#contactform">Contacts</AnchorLink>
                 </div>
             </div>
         </nav>
