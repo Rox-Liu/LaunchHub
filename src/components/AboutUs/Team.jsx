f2c1f802996fdd1fddfa4515f86fb7b1abadb7cf
Dominic <dominicwong09@gmail.com>
2023-07-02 01:33:51 +1200

    team page 90% done

--- src/components/AboutUs/Team.jsx
+++ src/components/AboutUs/Team.jsx
@@ -6,17 +6,8 @@ import Natalie from '../../images/hs-natalie.png';
 
 const Team = () => {
     const [ currentIndex, setCurrentIndex ] = useState(0);
+    const [ winWidth, setWinWidth ] = useState(0);
     
-    
-    useEffect( () => {
-
-        const interval = setInterval( () => {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % members.length);
-        }, 3000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     const members = [
         {
             image: Jess,
@@ -40,16 +31,58 @@ const Team = () => {
         }
     ];
 
+    const handleResize = () => {
+        setWinWidth(window.innerWidth);
+    };
+
+    useEffect( () => {
+        if (typeof window !== 'undefined') {
+            setWinWidth(window.innerWidth);
+            window.addEventListener('resize', handleResize);
+        }
+
+        return () => {
+            if (typeof window !== 'undefined') {
+                window.removeEventListener('resize', handleResize);
+            };
+        };
+    }, []);
+    
+    useEffect( () => {
+
+        const interval = setInterval( () => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % members.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+
     return (
         <div id="team-container">
             <div className="team-blurb">
                 <h1>Elevate your digital vision with us.</h1>
                 <p>Meet our team below</p>
             </div>
-            <div className="member-slides">
-                <img src={members[currentIndex].image } alt={members[currentIndex].name} />
-                <h2>{members[currentIndex].name}</h2>
-                <p>{members[currentIndex].role}</p>
+
+            <div id="member-container">
+                { winWidth > 1500 ? 
+                    members.map( (member) => {
+                        return (
+                            <div className="member-tiles">
+                                <img src={member.image } alt={member.name} />
+                                <h2>{member.name}</h2>
+                                <p>{member.role}</p>
+                            </div>
+                        )
+                    })
+                    :
+                    <div className="member-slides">
+                        <img src={members[currentIndex].image } alt={members[currentIndex].name} />
+                        <h2>{members[currentIndex].name}</h2>
+                        <p>{members[currentIndex].role}</p>
+                    </div>
+                }
             </div>
         </div>
     )
