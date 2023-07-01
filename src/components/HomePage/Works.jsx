0ac28d2f170c8258f01c37b772bf746e041c4059
Dominic <dominicwong09@gmail.com>
2023-07-01 16:57:55 +1200

    updated work css

--- src/components/HomePage/Works.jsx
+++ src/components/HomePage/Works.jsx
@@ -7,16 +7,19 @@ const Work = () => {
     const works = [
         {
             header: 'Our Works',
+            name: 'ClimateWorks Website',
             image: 'http://placekitten.com/400/800',
             desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error molestias illum, facere ipsa aperiam, adipisci beatae ipsum, obcaecati id odio? Deleniti tenetur quam quaerat. Sunt quae quis velit a?'
         },
         {  
             header: '',
+            name: 'ShelterLink Website',
             image: 'http://placekitten.com/401/801',
             desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error molestias illum, facere ipsa aperiam, adipisci beatae ipsum, obcaecati id odio? Deleniti tenetur quam quaerat. Sunt quae quis velit a?'
         },
         {
             header: '',
+            name: 'Carbon Collective App',
             image: 'http://placekitten.com/402/800',
             desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor error molestias illum, facere ipsa aperiam, adipisci beatae ipsum, obcaecati id odio? Deleniti tenetur quam quaerat. Sunt quae quis velit a?'
         }
@@ -32,6 +35,7 @@ const Work = () => {
                         </div>
                         <div className="work">
                             <div className="work-img" style={{ backgroundImage: `url(${work.image})`}}></div>
+                            <p>{work.name}</p>
                             <p>{work.desc}</p>
                         </div>
                     </div>
