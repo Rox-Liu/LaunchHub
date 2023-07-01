ee58f6134c76e7edf58e27d1cd86f3fc80fa7207
Roxanne Liu <roxanneliu819@gmail.com>
2023-07-01 21:07:44 +1000

    bug fixed, navbar works normal now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,17 +20,17 @@ const Navbar = () => {
             </label>
             <input type="checkbox" id="checkbox-menu"></input>
             <div className="menu-list">
-                <label htmlFor="checkbox-menu" className="label-menu">
+                <label htmlFor="checkbox-menu" className="label-menu label-menu--close">
                     <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 0 24 24" width="32px" fill="#E5692B">
                         <path d="M0 0h24v24H0V0z" fill="none"/>
                         <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
                     </svg>
                 </label>
                 <Link to="/">Home</Link>
-                <Link to="/about">About Us</Link>
+                <Link to="/about">About</Link>
                 <AnchorLink to="/#services">Services</AnchorLink>
                 <AnchorLink to="/#works">Works</AnchorLink>
-                <AnchorLink to="/#contactform">Contacts</AnchorLink>
+                <AnchorLink to="/#contactform">Contact</AnchorLink>
             </div>
         </nav>
     );
